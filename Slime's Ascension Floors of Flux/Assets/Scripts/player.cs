2143ec6bd2679da7a905af0477c5c0c5e6961673
Ruihaolol <ruihao111@gmail.com>
2023-05-22 20:57:29 +0200

    d

--- Slime's Ascension Floors of Flux/Assets/Scripts/player.cs
+++ Slime's Ascension Floors of Flux/Assets/Scripts/player.cs
@@ -5,38 +5,22 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     public basicMovement basicmovement;
-    public float maxStamina=50;
-    public float stamina=50;
+
     
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        if (basicmovement.slowdown == true) {
-            staminaCountdown();
-        }
-    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.tag == "death") { GameObject.Destroy(this.gameObject); }
     }
-    public void staminaCountdown() {
-        while (stamina >= 0f) { 
-            stamina = stamina - 0.4f; 
-       }
-        if (stamina == 0) {
-            basicmovement.slowdown = false;
-        }
-    }
-    private void OnCollisionExit2D(Collision2D collision)
+
+    // Update is called once per frame
+    void FixedUpdate()
     {
-        if (collision.collider.tag == "grounded"&& stamina<=maxStamina) {
-            stamina = stamina + 0.4f;
-        }
+
     }
+
 }
