d6dd796fa80a1e1c64f654467f4098218fb6d330
chaou1 <77403427+chaou1@users.noreply.github.com>
2023-05-23 17:22:41 +0200

    nothing

--- Slime's Ascension Floors of Flux/Assets/Scripts/roomS/roomSpawner.cs
+++ Slime's Ascension Floors of Flux/Assets/Scripts/roomS/roomSpawner.cs
@@ -10,6 +10,7 @@ public class roomSpawner : MonoBehaviour
     bool hasspawned;
     int randNumber;
     roomTemplate rooms;
+    
 
     // Start is called before the first frame update
     void Start()
