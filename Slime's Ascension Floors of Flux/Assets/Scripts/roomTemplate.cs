aebfb23cab865a399e7a312044e17df463f54c2a
chaou1 <77403427+chaou1@users.noreply.github.com>
2023-05-23 14:53:46 +0200

    Room generation

---
+++ Slime's Ascension Floors of Flux/Assets/Scripts/roomTemplate.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roomTemplate : MonoBehaviour
+{
+    public GameObject[] upRooms;
+    public GameObject[] rightRooms;
+    public GameObject[] downRooms;
+    public GameObject[] leftRooms;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
